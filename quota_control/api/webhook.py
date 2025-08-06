cf9e533dc40e903c282b8c47962ef318507cc038
alihusnain-dev <alihusnainmughal215@gmail.com>
2025-08-06 20:27:26 +0500

    read_quota_minor fix

--- quota_control/api/webhook.py
+++ quota_control/api/webhook.py
@@ -3,8 +3,8 @@ import json
 
 
 @frappe.whitelist(allow_guest=True)
-def read_quota():
-    site_name = 'local.tour'
+def read_quota(site_name=None):
+    # site_name = 'local.tour'
     if not site_name:
         return "sitename missing"
     doc = frappe.get_doc('ERPNext Site', site_name)
